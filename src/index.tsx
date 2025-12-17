fed2a3ddf0e24f6ffefd19eee4f51eaacad5e622
Shivamk1 <Shivamk1@damcogroup.com>
2025-12-17 15:31:31 +0530

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 const container = document.getElementById('root') as HTMLElement | null;
 if (!container) {
-  console.error('Root container with id "root" not found');
+  console.error('Root container with id "root" not found ');
 } else {
   const root = createRoot(container as HTMLElement);
   root.render(<App />);
